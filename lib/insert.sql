9fbfb066d1f004af1a1593a6e41008512f053d3e
iankuria668 <iankuria668@gmail.com>
2024-06-10 09:47:41 +0300

    Solved

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT  INTO bears (id, name, age, sex, color, temperament, alive) VALUES
+(9, 'Mr. Chocolate', 20, 'M', 'dark brown', 'calm', 0),
+(10, 'Rowdy', 10, 'M', 'black', 'intense', 1),
+(11, 'Tabitha', 8, 'F', 'black', 'calm', 1),
+(12, 'Sergeant Brown', 21, 'M', 'dark brown', 'intense', 1),
+(13, 'Melissa', 13, 'F', 'dark brown', 'goofy', 1),
+(14, 'Grinch', 2, 'M', 'Black', 'Grinchy', 1)
+(15, 'Wendy', 6, 'F', 'Blue', 'naive', 1)
+(9,  nulll, 20, 'M', 'black', 'aggressive', 0)
